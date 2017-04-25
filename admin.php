09d3f9e55731d4efcd953ecb6b4479078f2df7b4
suwenxing <869345286@qq.com>
2017-04-25 20:19:33 +0800

    1测试

--- admin.php
+++ admin.php
@@ -21,6 +21,6 @@ define('BIND_MODULE','Admin');
 define('APP_PATH','./Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+require './ThinkPHP/ThinkPHP.php'; 
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单

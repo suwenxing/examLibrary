a97a761de5f9758119ba8f708a4e4d647d4cb398
suwenxing <869345286@qq.com>
2017-04-26 10:49:30 +0800

    1、增加登陆界面

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,13 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //数据库配置
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'exam',     // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',          // 端口
+    'DB_PREFIX'             =>  'e_',          // 数据库表前缀
+    
+    'SHOW_PAGE_TRACE' =>true,
 );

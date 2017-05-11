b640e2983f30ed4352adbb45dc25a278e72cd79a
suwenxing <869345286@qq.com>
2017-05-11 09:09:39 +0800

    1

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -5,21 +5,6 @@ class LoginController extends Controller {
     public function index(){
         $this->display();
     }
-<<<<<<< HEAD
-    
- 	public function login(){
-    	$this->display();
-    }
-    public function answer(){
-    	$this->display();
-    	
-    }
-    public function score(){
-    	
-    	$this->display();
-    }
-=======
-
->>>>>>> branch 'master' of https://github.com/suwenxing/examLibrary.git
+ 
 }
 
